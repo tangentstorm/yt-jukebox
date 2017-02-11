ee1ba0ae05647fccb5aaefd92b6f2f82015ce89c
tangentstorm <michal.wallace@gmail.com>
2017-02-10 21:41:06 -0500

    hook up backend to firebase API

---
+++ firebase/firebase-init.in.js
@@ -0,0 +1,10 @@
+ // replace this file with the credentials from your own firebase account,
+ // and save it as 'firebase-init.js'.
+var config = {
+  apiKey: "{APIKEY}",
+  authDomain: "{AUTHDOMAIN}",
+  databaseURL: "{DBURL}",
+  storageBucket: "{BUCKET}",
+  messagingSenderId: "{SENDER}"
+};
+firebase.initializeApp(config);
